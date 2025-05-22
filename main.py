a2236e7d778098efda2b408efe93c10ef73a4d5f
Vladislav-re <v.kobelkov12@gmail.com>
2025-05-22 12:52:46 +0300

    update
    update

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-from tourist_video_bot.fetch_video import download_tourist_video
-from tourist_video_bot.caption_generator import generate_caption
-from tourist_video_bot.telegram_poster import post_to_telegram
-import os
+
+from fetch_video import download_tourist_video
+from telegram_poster import post_to_telegram
+from caption_generator import generate_caption
 
 def main():
-    video_path, location = download_tourist_video()
-    if video_path:
-        caption = generate_caption(location)
-        post_to_telegram(video_path, caption)
-        os.remove(video_path)
+    video_path, video_title = download_tourist_video()
+    caption = generate_caption(video_title)
+    post_to_telegram(video_path, caption)
 
 if __name__ == "__main__":
     main()
---
+++ telegram_poster.py
@@ -0,0 +1,13 @@
+
+import os
+from telegram import Bot
+
+def post_to_telegram(video_path, caption):
+    token = os.getenv("TELEGRAM_BOT_TOKEN")
+    channel_id = os.getenv("TELEGRAM_CHANNEL_ID")
+    bot = Bot(token=token)
+
+    with open(video_path, "rb") as video:
+        bot.send_video(chat_id=channel_id, video=video, caption=caption)
+
+    os.remove(video_path)
